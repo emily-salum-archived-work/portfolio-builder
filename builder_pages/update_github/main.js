9fbf8dad24b7e7e9bf4410ac688b2427eee11dab
EmilySalum <EmilyMarquesSalum@gmail.com>
2022-03-26 18:08:52 -0300

    Alphabetically sorted styles and github-init args update

--- builder_pages/update_github/main.js
+++ builder_pages/update_github/main.js
@@ -42,6 +42,49 @@ function open_window(html,win)
 }
 exports.open_window = open_window;
 
+
+
+
+/* Receives an array and turns it into string-formatted version of it.  */
+function stringifyArray(arr) {
+    let str = "[";
+    for (let i = 0; i < arr.length; i++) {
+
+        let element = arr[i];
+        
+        if(typeof element === "object") {
+            element = JSON.stringify(element);
+        }
+        str += element;
+        if (i != arr.length - 1) {
+            str += ",";
+        }
+    }
+    str += "]";
+    return str;
+}
+
+
+/* Receives an array of paths, returns an array with builderPath + path for each path; 
+    if the path is an object, it adds to the attribute "str" of that object instead. */
+function getBuilderPaths(initialPaths) {
+    const builderPath = "C:/Users/user/Desktop/emily/projects/@site_projects/portfolio-builder/";
+
+    let paths = [];
+    for (let i = 0; i < initialPaths.length; i++) {
+        let path = initialPaths[i];
+        if (typeof path == "object") {
+            path.str = builderPath + path.str;
+            paths.push(path);
+        } else {
+            paths.push(builderPath + path);
+        }
+   
+    }
+
+    return paths;
+}
+
 function send_changes() 
 {
     let {PythonShell} = require('python-shell');
@@ -50,18 +93,19 @@ function send_changes()
 
     const pythonPath = "C:\\Users\\user\\AppData\\Local\\Programs\\Python\\Python39\\python.exe";
 
+ 
+    /* Paths to every file that will be added to the portfolio repository */
+    let paths = getBuilderPaths([
+        "index.html", 
+    "bootstrap-5.1.0-dist", 
+    "portfolio_need", 
+    {"str": "README-PORTFOLIO.md", "replace": "README.md"},
+    "READMESP"]);
 
-    const builderPath = "C:/Users/user/Desktop/emily/projects/@site_projects/portfolio-builder/";
-
-    let args = '{"paths" : ["' + builderPath + 'index.html", '+
-           '"' + builderPath + 'bootstrap-5.1.0-dist",'+
-           ' "'+  builderPath + 'portfolio_need", "'  + 
-           builderPath + 'READMESP' + '",' +
-           ' "'+  builderPath + 'README-PORTFOLIO.md" '  + '] }';
+    let args = '{"paths" : '+ JSON.stringify(paths) + '}';
  
     console.log(args);
-
-    console.log(args[15]);
+ 
 
     if (changes_sent){ return;}
     changes_sent = true;
